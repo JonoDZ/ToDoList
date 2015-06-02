5198e53cf3570697cd4a93b3a9f11aaa214097c9
JonoDZ <jzac90@gmail.com>
2015-06-02 18:38:58 +0100

    onkeyup - bug with onblue

---
+++ js/script from review.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+/*/////////////////
+Frameworks and notes
+*//////////////////
+
+//garbage collection - remove references to be collected (eventlistener)
+//accessability (img/a) - over span
+	//Hide text from image
+	//Marwan link to article
+	//Keyboard only navigation
+	//reordering with only keyboard
+	//keybaord shortcuts / keyboard control (start here)
+	//blind - deaf
+	//aria roles - research intro
+
+//contenteditable - editing list items
+//dragging list order - DOM events - drag?
+//local storage over cookies (API)
+// remove ID - (look up data atribute)
+//books - you dont know JS
+
+/*
+done:
+	functionality
+		line through - if statement under pre-existing eventlistener
+		reset button
+
+
