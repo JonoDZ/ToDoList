2a76249a5f882cceb941de0bcae2afbdea5f6660
Jonathon Zachariah <jzac90@gmail.com>
2015-12-16 12:52:52 +0000

    refactored further parent/child nodes; removed cookie framework to use local storage

--- js/script.js
+++ js/script.js
@@ -18,8 +18,11 @@ var list = document.getElementById("todoList");
 		//Cross' out text if lineButton is clicked 
 		if (el.childNodes[0].className === 'lineButton') {
 
+			//assign entire current list entry
+			var currentLiItem = el.parentNode;
+
 			//store the text's className as string
-			var textClassName = el.parentNode.childNodes[0].className;
+			var textClassName = currentLiItem.childNodes[0].className;
 
 			//search for "lineThrough" class against current class', store results
 			var searchResultsLineThrough = textClassName.search("linethrough");
@@ -27,13 +30,13 @@ var list = document.getElementById("todoList");
 			//if lineThrough Class is present in search results
 			if (searchResultsLineThrough != -1) {
 				//remove the lineThrough Class, apply noLineThrough Class
-				el.parentNode.childNodes[0].className = textClassName.replace("linethrough", "noLinethrough");
+				currentLiItem.childNodes[0].className = textClassName.replace("linethrough", "noLinethrough");
 			}
 
 			//if lineThrough Class is not present in search results
 			else if (searchResultsLineThrough = -1) {
 				//remove the noLineThrough Class, apply lineThrough Class
-				el.parentNode.childNodes[0].className = textClassName.replace("noLinethrough", "linethrough");
+				currentLiItem.childNodes[0].className = textClassName.replace("noLinethrough", "linethrough");
 			}
 
 		}
@@ -113,12 +116,17 @@ var list = document.getElementById("todoList");
 	}
 
 	saveButton.onclick = function () {
-		docCookies.setItem('saveCookie', document.getElementById("todoList").innerHTML);
+					
+		// Put the ToDoList into Localstorage as string
+		localStorage.setItem('ToDoListHTML', JSON.stringify(document.getElementById("todoList").innerHTML));
+
 		inputText.focus();	
 	}
 
 	loadButton.onclick = function () {
-		document.getElementById("todoList").innerHTML = docCookies.getItem('saveCookie');
+
+		//Parse String, load HTML back into document.
+		document.getElementById("todoList").innerHTML = JSON.parse(localStorage.getItem('ToDoListHTML'));
 		inputText.focus();
 	}
 
@@ -174,70 +182,3 @@ var list = document.getElementById("todoList");
 		anchorElement.appendChild(imgElement);
 		listItem.appendChild(anchorElement);
 	};
-
-
-//below framework to be removed once a suitable alternatative to cookie storage is found. 
-
-/*\    
-|*|
-|*|  :: cookies.js ::
-|*|
-|*|  A complete cookies reader/writer framework with full unicode support.
-|*|
-|*|  Revision #1 - September 4, 2014
-|*|
-|*|  https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
-|*|  https://developer.mozilla.org/User:fusionchess
-|*|
-|*|  This framework is released under the GNU Public License, version 3 or later.
-|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
-|*|
-|*|  Syntaxes:
-|*|
-|*|  * docCookies.setItem(name, value[, end[, path[, domain[, secure]]]])
-|*|  * docCookies.getItem(name)
-|*|  * docCookies.removeItem(name[, path[, domain]])
-|*|  * docCookies.hasItem(name)
-|*|  * docCookies.keys()
-|*|
-\*/
-
-var docCookies = {
-  getItem: function (sKey) {
-    if (!sKey) { return null; }
-    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-  },
-  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
-        case Number:
-          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-          break;
-        case String:
-          sExpires = "; expires=" + vEnd;
-          break;
-        case Date:
-          sExpires = "; expires=" + vEnd.toUTCString();
-          break;
-      }
-    }
-    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
-    return true;
-  },
-  removeItem: function (sKey, sPath, sDomain) {
-    if (!this.hasItem(sKey)) { return false; }
-    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-    return true;
-  },
-  hasItem: function (sKey) {
-    if (!sKey) { return false; }
-    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-  },
-  keys: function () {
-    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-    for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
-    return aKeys;
-  }
-};
